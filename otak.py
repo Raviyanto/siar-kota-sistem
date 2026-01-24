b9163e79d56f0ff1c36d0df8db002140fc110f71
Raviyanto <raviyanto@gmail.com>
2026-01-23 23:42:21 -0500

    Tambah qwebchannel

--- otak.py
+++ otak.py
@@ -2,24 +2,36 @@ import sys
 import os
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtWebEngineWidgets import QWebEngineView
-from PyQt6.QtCore import QUrl
+from PyQt6.QtWebChannel import QWebChannel
+from PyQt6.QtCore import QUrl, QObject, pyqtSlot
 
-# 1. Setup Aplikasi
+# --- BAGIAN SARAF (LOGIKA BACKEND) ---
+class Jembatan(QObject):
+    @pyqtSlot(str)
+    def terima_perintah(self, pesan):
+        print(f"📡 DITERIMA: {pesan}")
+        
+        # Logika: Jika tombol OFF ditekan
+        if pesan == "tutup_aplikasi":
+            print("Mematikan Sistem...")
+            sys.exit() # Tutup aplikasi
+
+# --- BAGIAN UTAMA ---
 app = QApplication(sys.argv)
-
-# 2. Setup Browser
 browser = QWebEngineView()
 
-# 3. Tentukan Lokasi File HTML Lokal
-# (Ini trik agar Python tahu di mana index.html berada secara otomatis)
+# 1. Siapkan Jembatan
+channel = QWebChannel()
+handler = Jembatan()
+channel.registerObject('jembatan_sistem', handler) # Nama ini penting!
+
+# 2. Pasang Jembatan ke Browser
+browser.page().setWebChannel(channel)
+
+# 3. Muat HTML
 path_sekarang = os.path.dirname(os.path.abspath(__file__))
 path_html = os.path.join(path_sekarang, "aset/index.html")
-
-# 4. Muat File HTML
 browser.load(QUrl.fromLocalFile(path_html))
 
-# 5. Tampilkan Full Screen
 browser.showFullScreen()
-
-# 6. Eksekusi
 sys.exit(app.exec())
